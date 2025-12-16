020ddb4b44a18dad1d7e35d48fb6287324c7a1b3
Sebasjpk4 <sebasjpk4@gmail.com>
2025-12-16 01:17:14 -0500

    cambios-2

--- frontend/react-app/src/App.tsx
+++ frontend/react-app/src/App.tsx
@@ -1,70 +1,18 @@
-import Card from "./components/Card.tsx";
-import List from "./components/List.tsx";
-import Button from "./components/Button";
-import { useState } from "react";
+// import Card from "./components/Card.tsx";
+// import List from "./components/List.tsx";
+// import Button from "./components/Button";
+// import { useState } from "react";
 
 function App() {
-  const [data, setData] = useState(["sebas 1"]);
-
-  // const [isLoaded, setIsLoaded] = useState(false);
-
-  // const handleClick = () => {
-  //   setIsLoaded(!isLoaded);
-  // };
-
-  // const list: string[] = ["sebas 1"];
-
-  // const handleAdd = () => {
-  //   console.log("Agregar elemento");
-  //   list.push("minion");
-  // };
-
-  // const handleDel = () => {
-  //   console.log("Eliminar elemento");
-  //   list.pop();
-  // };
-
-  // const handleSelect = (elemento: string) => {
-  //   console.log("Seleccionado:", elemento);
-  // };
-
-  // const contenido = list.length ? (
-  //   <List data={list} onSelect={handleSelect} />
-  // ) : (
-  //   <p>No hay elementos para mostrar</p>
-  // );
-
-  // return (
-  //   <Card>
-  //     <CardBody title="Hola " text="Este es un texto de ejemplo."></CardBody>
-  //     {contenido}
-
-  //     <Button isLoading={isLoaded} onClick={handleClick}>
-  //       {isLoaded ? "Cargando..." : "Hola Mundo"}
-  //     </Button>
-
-  //     <Button onClick={handleAdd} add>
-  //       Agregar
-  //     </Button>
-  //     <Button onClick={handleDel} del>
-  //       Eliminar
-  //     </Button>
-  //   </Card>
-  // );
-  const adder = () => {
-    setData([...data, `sebas ${data.length + 1}`]);
-  };
-
-  const del = () => {
-    setData(data.slice(0, -1));
-  };
   return (
-    <Card>
-      <Button onClick={adder}>Agregar</Button>
-      <Button onClick={del}>Eliminar</Button>
-
-      <List data={data} />
-    </Card>
+    <div className="App">
+      {/* <h1>Hola, Encanto Paluato!</h1>
+      <Button text="Click me" onClick={() => alert("Button clicked!")} />
+      <List
+        items={["Item 1", "Item 2", "Item 3"]}
+        renderItem={(item) => <Card title={item} content={`Content for ${item}`} />}
+      /> */}
+    </div>
   );
 }
 
--- frontend/react-app/src/main.tsx
+++ frontend/react-app/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // @vite-ignore
 import App from "./App.tsx";
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 // React DOM se encarga de renderizar los componentes de React en el DOM del navegador
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
